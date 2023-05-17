d0483b4af217e6d43c6db77a16a8d0fe94a44711
Joshua Alexander <joshuaalexandertb@Joshuas-MacBook-Air.local>
2023-05-16 21:59:26 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('.display');
+const clear = document.querySelector('.clear')
+const cancel = document.querySelector('.delete')
+const operator = document.querySelector('.divide')
+const multiply = document.querySelector('.multiply')
+const minus = document.querySelector('.minus')
+const plus = document.querySelector('.plus')
+const equals = document.querySelector('.equals')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+const three = document.querySelector('.three')
+const four = document.querySelector('.four')
+const five = document.querySelector('.five')
+const six = document.querySelector('.six')
+const seven = document.querySelector('.seven')
+const eight = document.querySelector('.eight')
+const nine = document.querySelector('.nine')
+const zero = document.querySelector('.zero')
+
+function clear {
+
+}
+clear.addEventListener("click", )
+
